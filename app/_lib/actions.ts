165366eb78e3059877e1c14263b6a229c332e9b9
Mohammad Lashani <sapumr@gmail.com>
2024-06-09 20:03:07 +0330

    add revalidatePath to clear browser cache after updating guest in account/profile page

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -24,6 +25,8 @@ export async function updateGuest(formData: FormData) {
     .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
