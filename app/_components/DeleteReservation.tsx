8ecfde43a5c3533903d9517c937f2e5d2f1585a1
Mohammad Lashani <sapumr@gmail.com>
2024-06-09 21:25:51 +0330

    adding spinner to DeleteReservation button with useTransition

--- app/_components/DeleteReservation.tsx
+++ app/_components/DeleteReservation.tsx
@@ -2,17 +2,34 @@
 
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { deleteReservation } from "../_lib/actions";
+import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 type DeleteReservationProps = { bookingId: string };
 
 function DeleteReservation({ bookingId }: DeleteReservationProps) {
+  const [isPending, startTransition] = useTransition();
+
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this reservation?"))
+      startTransition(() => deleteReservation(bookingId));
+  }
+
   return (
     <button
-      onClick={() => deleteReservation(bookingId)}
+      onClick={handleDelete}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
